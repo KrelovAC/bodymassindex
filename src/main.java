00c7658743d9d6b35b8d76135eab677c7ff8139f
AKrelov <wsup@bk.ru>
2022-11-19 21:53:17 +0300

    sec commit

--- src/main.java
+++ src/main.java
@@ -2,8 +2,8 @@ public class main {
     public static void main(String[] args) {
         BodyMassIndex service = new BodyMassIndex();
 
-        double mass = 40;
-        double hight = 1.67;
+        double mass = 66;
+        double hight = 1.55;
         double bmi = service.calculate(mass,hight);
         System.out.println(bmi + " - индекс массы тела");
 
