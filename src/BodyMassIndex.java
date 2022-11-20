be021274bd22c37d9a55092a948543af0d2102a6
AKrelov <wsup@bk.ru>
2022-11-20 13:01:56 +0300

    new commit

--- src/BodyMassIndex.java
+++ src/BodyMassIndex.java
@@ -1,7 +1,7 @@
 public class BodyMassIndex {
     public double calculate(double mass, double hight){
         double f;
-        f = hight * hight;
+        f = Math.pow(hight,2); //f = hight * hight;
         double s;
         s = mass / f;
         return s;
--- src/main.java
+++ src/main.java
@@ -2,9 +2,10 @@ public class main {
     public static void main(String[] args) {
         BodyMassIndex service = new BodyMassIndex();
 
-        double mass = 66;
-        double hight = 1.55;
+        double mass = 83;
+        double hight = 1.8;
         double bmi = service.calculate(mass,hight);
+        System.out.println();
         System.out.println(bmi + " - индекс массы тела");
 
 
