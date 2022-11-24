b0d77a0bbe6891861de6bfc99303db602d4ad971
AKrelov <wsup@bk.ru>
2022-11-24 13:12:02 +0300

    sec commit

--- src/BodyMassIndex.java
+++ src/BodyMassIndex.java
@@ -1,9 +1,8 @@
 public class BodyMassIndex {
     public double calculate(double mass, double hight){
-        double f;
-        f = Math.pow(hight,2); //f = hight * hight;
-        double s;
-        s = mass / f;
-        return s;
+        double expo = Math.pow(hight,2);
+        double div = mass / expo;
+
+        return div;
     }
 }
